06f2de1a51fb43c72904f52fb78db806862b168e
Joao VIctor Martinelli <jv@Joaos-MacBook-Pro.local>
2022-09-12 22:43:04 -0300

    removing comments

--- src/connection/mongo.ts
+++ src/connection/mongo.ts
@@ -7,7 +7,6 @@ const connectToDatabase = async () => {
 
   const db = mongoose.connection;
 
-  // Bind connection to error event (to get notification of connection errors)
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 };
 
